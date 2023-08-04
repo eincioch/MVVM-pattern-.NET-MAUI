a8cd00e43d0eb3dd2702dae5c90ffd435bea4da7
Pieter Nijs <pieternijs@live.be>
2023-08-04 15:28:31 +0200

    Finished Code Chapter 8

---
+++ Chapter 08/Finish/Recipes App/Recipes.Client.Core/Navigation/INavigationService.cs
@@ -0,0 +1,11 @@
+﻿using Recipes.Client.Core.Recipes;
+
+namespace Recipes.Client.Core.Navigation;
+
+public interface INavigationService
+{
+    Task GoToOverview();
+    Task GoToRecipeDetail(string recipeId);
+    Task GoToRecipeRatingDetail(RecipeDetailDto recipe);
+    Task GoBack();
+}
